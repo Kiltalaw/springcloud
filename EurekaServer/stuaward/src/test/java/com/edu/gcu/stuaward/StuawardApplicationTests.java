ffb36dd35c7c5411a1fcd7110632c9970289f8d3
Kiltalaw <1029110893@qq.com>
2019-01-12 18:50:41 +0800

    EurekaServer
    EurekaServer

---
+++ EurekaServer/stuaward/src/test/java/com/edu/gcu/stuaward/StuawardApplicationTests.java
@@ -0,0 +1,17 @@
+package com.edu.gcu.stuaward;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StuawardApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
